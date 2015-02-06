d1945627e5ad4a0f3b61198ff7467fcc97944fab
sloanyang <sloanyang@gmail.com>
2015-02-06 11:46:56 +0800

    Upload an new skill in kernel

--- freemind/kernel.mm
+++ freemind/kernel.mm
@@ -1,1 +1,25 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?><map version="0.8.1"><node CREATED="1395388842933" ID="514rjeko8b6gnq7tbq71g2rv0k" MODIFIED="1395388842933" TEXT="kernel"><node CREATED="1395388842933" ID="5ue374i6v4skq9maiiucaupilg" MODIFIED="1395388842933" POSITION="right" TEXT="embedded system"><node CREATED="1395388842933" ID="7fttl80b06rfm3mn4se4s5rm3p" MODIFIED="1395388842933" TEXT="GTK"/><node CREATED="1395388842933" ID="7pcfvre2385j3k6tm7fin7s1i9" MODIFIED="1395388842933" TEXT="Qt"/><node CREATED="1395388842933" ID="1m9fkhva8li5fd9v5r7qo3ajja" MODIFIED="1395388842933" TEXT="X"/><node CREATED="1395388842934" ID="4r2ckcknnk1bdnadhi71cqanm3" MODIFIED="1395388842934" TEXT="RDP"/></node><node CREATED="1395388842934" ID="7ntdtgokib9tf1u7o36i621et1" MODIFIED="1395388842934" POSITION="right" TEXT="2D graphics"><node CREATED="1395388842934" ID="5tk718i7s4krs7efd0tu62avuq" MODIFIED="1395388842934" TEXT="D/E/Blitter HW"/></node><node CREATED="1395388842934" ID="3n33gu2dn0inqicr83i5gk2stl" MODIFIED="1395388842934" POSITION="right" TEXT="3D graphics"><node CREATED="1395388842934" ID="4vof0ubud3kugbg24br2krhcqr" MODIFIED="1395388842934" TEXT="VideoCore VI"/></node><node CREATED="1395388842934" ID="5mutgs1lnjbv9g2b5iu6q3pfa6" MODIFIED="1395388842934" POSITION="right" TEXT="PCI/device drivers"><node CREATED="1395388842934" ID="0m3vreca7jgc25imorv7b69ob6" MODIFIED="1395388842934" TEXT="PCI HW/Verilog"/></node><node CREATED="1395388842934" ID="4lgutnhhagf3nb8km5ffrnvdp9" MODIFIED="1395388842934" POSITION="left" TEXT="boot"><node CREATED="1395388842934" ID="5ph0ld9muv2jm16eh9g0n3sgk5" MODIFIED="1395388842934" TEXT="ARM Arch/Assembly"/></node><node CREATED="1395388842934" ID="426oqkqkipucefomvuurh8abpt" MODIFIED="1395388842934" POSITION="left" TEXT="bionic/eglibc"/></node></map>
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1423194181788" ID="7a531kd961l360fc9vr4gvb300" MODIFIED="1423194354940" TEXT="kernel">
+<node CREATED="1423194181788" ID="4fbn1mssdm0qlpcro09701c2mk" MODIFIED="1423194181788" POSITION="right" TEXT="embedded system">
+<node CREATED="1423194181788" ID="0o2dh0hl6apuatkpdolhhihhc8" MODIFIED="1423194181788" TEXT="GTK"/>
+<node CREATED="1423194181788" ID="62unblb6ihngq7i8t0ug8p17ck" MODIFIED="1423194181788" TEXT="Qt"/>
+<node CREATED="1423194181788" ID="2lqvvrgvf89oqu1ub0ujdt4s5r" MODIFIED="1423194181788" TEXT="X"/>
+<node CREATED="1423194181788" ID="3kuqutjraeac4bv1dqvd05610m" MODIFIED="1423194181788" TEXT="RDP"/>
+</node>
+<node CREATED="1423194181788" ID="1msv1t84r5lm7bhmriuf9h2ojq" MODIFIED="1423194181788" POSITION="right" TEXT="2D graphics">
+<node CREATED="1423194181788" ID="7ku18su4knd8a54j6gogd59bhd" MODIFIED="1423194181788" TEXT="D/E/Blitter HW"/>
+</node>
+<node CREATED="1423194181788" ID="34ul7jb3pod1vj7sqmgaslatl7" MODIFIED="1423194181788" POSITION="right" TEXT="3D graphics">
+<node CREATED="1423194181788" ID="766gihkssk0ktot1bj7n86flnb" MODIFIED="1423194181788" TEXT="VideoCore VI"/>
+</node>
+<node CREATED="1423194181788" ID="0ughbo7rtkg32dk4r006g25m4e" MODIFIED="1423194181788" POSITION="right" TEXT="PCI/device drivers">
+<node CREATED="1423194181788" ID="62opnn7g1716kc9ch6atq8qjt6" MODIFIED="1423194181788" TEXT="PCI HW/Verilog"/>
+</node>
+<node CREATED="1423194181788" ID="5kocl1bcq2giemafrhfiv61262" MODIFIED="1423194181788" POSITION="left" TEXT="boot">
+<node CREATED="1423194181788" ID="2jkrd7eqtcm9krfcgk0pvsd0mn" MODIFIED="1423194181788" TEXT="ARM Arch/Assembly"/>
+</node>
+<node CREATED="1423194181788" ID="0do578eohd3tekbe1pkfm3p4ei" MODIFIED="1423194181788" POSITION="left" TEXT="bionic/eglibc"/>
+<node CREATED="1423194181788" ID="12shu2a0s0hh3hnp7g8gkpj14n" MODIFIED="1423194181788" POSITION="left" TEXT="trace &amp; injection"/>
+</node>
+</map>
